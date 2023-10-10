98feb9af41a11dc822eff3a9240b79c1b975baac
danielcamucatto <daniel.camucatto@gmail.com>
2023-10-09 22:35:04 -0300

    resolvendo layout e logica

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -1,49 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer2, ElementRef } from '@angular/core';
+import { faChevronLeft, faChevronRight, faPlusCircle } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-gallery',
   templateUrl: './gallery.component.html',
   styleUrls: ['./gallery.component.scss']
 })
-export class GalleryComponent implements OnInit {
 
-  constructor() { }
+export class GalleryComponent implements OnInit {
+  faChevronLeft = faChevronLeft;
+  faPlusCircle = faPlusCircle;
+
+  constructor(private renderer: Renderer2, private el: ElementRef) { }
 
   ngOnInit(): void {
-    const imgWrappers = document.querySelectorAll(".img-wrapper");
+    const imgWrappers = this.el.nativeElement.querySelectorAll(".img-wrapper");
+
     imgWrappers.forEach((imgWrapper: Element) => {
-      imgWrapper.addEventListener('mouseenter', () => {
-        const imgOverlay = imgWrapper.querySelector(".img-overlay") as HTMLElement;
-        if (imgOverlay) {
-          imgOverlay.animate({ opacity: 1 }, 600);
-        }
+      const imgOverlay = imgWrapper.querySelector(".img-overlay") as HTMLElement;
+
+      this.renderer.listen(imgWrapper, 'mouseenter', () => {
+        this.renderer.setStyle(imgOverlay, 'opacity', '1');
       });
 
-      imgWrapper.addEventListener('mouseleave', () => {
-        const imgOverlay = imgWrapper.querySelector(".img-overlay") as HTMLElement;
-        if (imgOverlay) {
-          imgOverlay.animate({ opacity: 0 }, 600);
-        }
+      this.renderer.listen(imgWrapper, 'mouseleave', () => {
+        this.renderer.setStyle(imgOverlay, 'opacity', '0');
       });
     });
 
     const overlay = document.createElement('div');
     overlay.id = 'overlay';
     const image = new Image();
-    const prevButton = document.createElement('div');
-    prevButton.id = 'prevButton';
-    prevButton.innerHTML = '<i class="fa fa-chevron-left"></i>';
-    const nextButton = document.createElement('div');
-    nextButton.id = 'nextButton';
-    nextButton.innerHTML = '<i class="fa fa-chevron-right"></i>';
-    const exitButton = document.createElement('div');
-    exitButton.id = 'exitButton';
-    exitButton.innerHTML = '<i class="fa fa-times"></i>';
 
     overlay.appendChild(image);
-    overlay.appendChild(prevButton);
-    overlay.appendChild(nextButton);
-    overlay.appendChild(exitButton);
 
     const gallery = document.getElementById('gallery');
     if (gallery) {
@@ -52,36 +41,51 @@ export class GalleryComponent implements OnInit {
 
     overlay.style.display = 'none';
 
-    const imgOverlays = document.querySelectorAll(".img-overlay");
+    const imgOverlays = this.el.nativeElement.querySelectorAll(".img-overlay");
     imgOverlays.forEach((imgOverlay: Element) => {
-      imgOverlay.addEventListener('click', (event) => {
+      this.renderer.listen(imgOverlay, 'click', (event) => {
         event.preventDefault();
         const imageLocation = (imgOverlay.previousElementSibling as HTMLAnchorElement)?.getAttribute("href");
         if (imageLocation) {
           image.src = imageLocation;
-          overlay.style.display = 'block';
+          overlay.style.display = 'flex';
+          overlay.style.justifyContent = 'center';
+          overlay.style.alignItems = 'center';
+          overlay.style.background = 'rgba(0, 0, 0, 0.7)';
+          overlay.style.position = 'absolute';
+          overlay.style.top = '50%'; 
+          overlay.style.left = '50%'; 
+          overlay.style.transform = 'translate(-50%, -50%)'; 
+          overlay.style.right = '0';
+          overlay.style.bottom = '0';
+          overlay.style.zIndex = '999';
+          overlay.style.borderRadius = '5px';
+          overlay.style.width = '100%'; 
+          overlay.style.height = '100%';
+          overlay.classList.add('visible');
         }
       });
     });
 
-    overlay.addEventListener('click', () => {
+    this.renderer.listen(overlay, 'click', () => {
       overlay.style.display = 'none';
+      overlay.classList.remove('visible');
     });
 
     const nextButtonClicked = () => {
       const currentImage = document.querySelector("#overlay img") as HTMLElement;
       if (currentImage) {
-        currentImage.style.display = 'none';
+        this.renderer.setStyle(currentImage, 'display', 'none');
         const currentImageSrc = currentImage.getAttribute("src");
         const currentImageElement = document.querySelector(`#image-gallery img[src="${currentImageSrc}"]`) as HTMLImageElement;
         const nextImageElement = currentImageElement?.closest(".image")?.nextElementSibling?.querySelector("img") as HTMLImageElement;
         const images = document.querySelectorAll("#image-gallery img");
         if (nextImageElement) {
-          (document.querySelector("#overlay img") as HTMLImageElement).src = nextImageElement.src;
-          (document.querySelector("#overlay img") as HTMLImageElement).style.display = 'block';
+          this.renderer.setAttribute(image, 'src', nextImageElement.src);
+          this.renderer.setStyle(image, 'display', 'block');
         } else {
-          (document.querySelector("#overlay img") as HTMLImageElement).src = (images[0] as HTMLImageElement).src;
-          (document.querySelector("#overlay img") as HTMLImageElement).style.display = 'block';
+          this.renderer.setAttribute(image, 'src', (images[0] as HTMLImageElement).src);
+          this.renderer.setStyle(image, 'display', 'flex');
         }
       }
     };
@@ -89,22 +93,22 @@ export class GalleryComponent implements OnInit {
     const prevButtonClicked = () => {
       const currentImage = document.querySelector("#overlay img") as HTMLElement;
       if (currentImage) {
-        currentImage.style.display = 'none';
+        this.renderer.setStyle(currentImage, 'display', 'none');
         const currentImageSrc = currentImage.getAttribute("src");
         const currentImageElement = document.querySelector(`#image-gallery img[src="${currentImageSrc}"]`) as HTMLImageElement;
         const prevImageElement = currentImageElement?.closest(".image")?.previousElementSibling?.querySelector("img") as HTMLImageElement;
         if (prevImageElement) {
-          (document.querySelector("#overlay img") as HTMLImageElement).src = prevImageElement.src;
-          (document.querySelector("#overlay img") as HTMLImageElement).style.display = 'block';
+          this.renderer.setAttribute(image, 'src', prevImageElement.src);
+          this.renderer.setStyle(image, 'display', 'flex');
         }
       }
     };
 
-    prevButton.addEventListener('click', prevButtonClicked);
-    nextButton.addEventListener('click', nextButtonClicked);
-
-    exitButton.addEventListener('click', () => {
-      overlay.style.display = 'none';
+    this.renderer.listen(document.body, 'click', (event) => {
+      if (event.target === overlay) {
+        overlay.style.display = 'none';
+        overlay.classList.remove('visible');
+      }
     });
   }
 }
